2a82ebc0bf15abcf438a5532b143c28922598ef2
Sean Kim <sean.m.kim@gmail.com>
2021-01-13 11:26:16 -0800

    Added docstring to sqlForPartialUdpate and tested it

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,21 +1,27 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
-// Inputs received:
-// Data may include:
-//   { firstName:, lastName, password, email, isAdmin }
-// jsToSql = {firstName: "first_name",
-//            lastName: "last_name",
-//            isAdmin: "is_admin",
-//            }
-// Data sent:
-// {firstName: 'Aliya', age: 32}
-//
-// Returning:
-// {
-//  setCols: ['"first_name"=$1', '"age"=$2'],
-//  values: ['Aliya', 32],
-// }
+/* Helper function for mapping data into SQL format for SET clause 
+in SQL UPDATE queries. Used in User and Company models.
+
+dataToUpate may include:
+  { firstName, lastName, password, email, isAdmin }
+
+  
+  EXAMPLE:
+    dataToUpdate = {firstName: 'Aliya', age: 32}
+    
+    jsToSql = {
+                firstName: "first_name",
+                lastName: "last_name",
+                isAdmin: "is_admin",
+            }
+    
+    Returning:
+      {
+        setCols: '"first_name"=$1', '"age"=$2',
+        values: ['Aliya', 32],
+      }
+  */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
---
+++ helpers/sql.test.js
@@ -0,0 +1,33 @@
+const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require("../expressError");
+
+describe("sqlForPartialUpdate", function () {
+  test("returns setCols and values in correct format", function () {
+    const dataToUpdate = { firstName: 'Aliya', age: 32 };
+    const jsToSql = {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    };
+    
+    const result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+
+    expect(result).toEqual({
+      setCols: "\"first_name\"=$1, \"age\"=$2",
+      values: ['Aliya', 32],
+    });
+  });
+
+  test("returns a BadRequestError if no data", function () {
+    const dataToUpdate = {};
+    const jsToSql = {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    };
+    
+    // Thunk - delay evaluation of a function
+    expect(() => (sqlForPartialUpdate(dataToUpdate, jsToSql))).toThrow(BadRequestError);
+  });
+
+});
